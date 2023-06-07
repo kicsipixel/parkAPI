b55ecf297eaa8bff909f2a0f362dcd37173e0304
Szabolcs Tóth <tsz@purzelbaum.hu>
2023-06-07 23:23:14 +0200

    Fixed: PostgreSQL init and README.md.

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 This is the code for the [https://github.com/hummingbird-project/hummingbird](Hummingbird) tutorial. It is a lightweight, flexible server framework written in Swift. Hummingbird consists of three main components, the core HTTP server, a minimal web application framework and the extension modules.
 
 After cloning the repo, you can run the server: ```swift run parkAPI``.
+
+This branch uses PostgreSQL database, so either you need to install it on your computer or you need to run: ```docker-compose up```.
--- Sources/parkAPI/Database/DatabaseSetup.swift
+++ Sources/parkAPI/Database/DatabaseSetup.swift
@@ -30,27 +30,3 @@ extension HBApplication {
         )
     }
 }
-//extension HBApplication {
-//    func setupDatabase() async throws {
-//        let path = "./hb-parks.sqlite"
-//        services.setUpSQLiteDatabase(
-//            path: path,
-//            threadPool: threadPool,
-//            eventLoopGroup: eventLoopGroup,
-//            logger: logger
-//        )
-//
-//        try await db.execute(
-//            .init(unsafeSQL:
-//                           """
-//                           CREATE TABLE IF NOT EXISTS parks (
-//                               "id" uuid PRIMARY KEY,
-//                               "latitude" double NOT NULL,
-//                               "longitude" double NOT NULL,
-//                               "name" text NOT NULL
-//                           );
-//                           """
-//                 )
-//        )
-//    }
-//}
